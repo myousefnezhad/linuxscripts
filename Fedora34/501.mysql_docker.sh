da59e6de6db77926d380c00f8ba5d606745e03ef
Tony Muhammad Yousefnezhad <myousefnezhad@gmail.com>
2021-06-13 22:34:20 -0600

    docker

--- Fedora34/501.mysql_docker.sh
+++ Fedora34/501.mysql_docker.sh
@@ -1,2 +1,5 @@
+sudo mkdir -p /etc/docker_mysql/
+sudo cp ../ymls/mysql.yml /etc/docker_mysql/docker-compose.yml
 sudo cp ../services/docker-mysql.service  /etc/systemd/system/.
-sudo systemctl enable --now docker-mysql
+sudo systemctl enable docker-mysql
+sudo systemctl start  docker-mysql
