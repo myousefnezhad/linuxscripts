8f4259842dc0ba8a94f5982a4af13fe810ffe21b
Tony Muhammad Yousefnezhad <myousefnezhad@gmail.com>
2021-06-14 00:14:14 -0600

    docker mysql phpMyAdmin

--- Fedora34/501.mysql_docker.sh
+++ Fedora34/501.mysql_docker.sh
@@ -1,5 +1,12 @@
-sudo mkdir -p /etc/docker_mysql/
-sudo cp ../ymls/mysql.yml /etc/docker_mysql/docker-compose.yml
-sudo cp ../services/docker-mysql.service  /etc/systemd/system/.
-sudo systemctl enable docker-mysql
-sudo systemctl start  docker-mysql
+sudo dns install docker docker-compose
+sudo systemctl enable docker
+sudo systemctl start  docker
+sudo docker pull mysql:latest
+sudo docker pull phpmyadmin/phpmyadmin
+sudo docker run -d --name mysql-server -v mysql-data:/var/lib/mysql --network=phpmyadmin-network -e "MYSQL_ROOT_PASSWORD=P@SsW0rD" -p 6033:3306 --restart always mysql:latest
+sudo docker run -d --name phpmyadmin -p 8081:80 --network phpmyadmin-network -e PMA_HOST=mysql-server --restart always phpmyadmin/phpmyadmin
+#sudo mkdir -p /etc/docker_mysql/
+#sudo cp ../ymls/mysql.yml /etc/docker_mysql/docker-compose.yml
+#sudo cp ../services/docker-mysql.service  /etc/systemd/system/.
+#sudo systemctl enable docker-mysql
+#sudo systemctl start  docker-mysql
