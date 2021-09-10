e09af0b34cf59fdd4182085821a04d9c8d064ecb
Tony Muhammad Yousefnezhad <myousefnezhad@gmail.com>
2021-09-10 15:00:34 -0600

    Moving from Debian 10 to 11

--- Debian-testing/011.nvidia_driver.sh
+++ Debian-testing/011.nvidia_driver.sh
@@ -1,6 +1,6 @@
 sudo apt update
-sudo apt install linux-headers-amd64
-sudo apt install nvidia-driver firmware-misc-nonfree
+sudo apt install -y linux-headers-amd64
+sudo apt install -y nvidia-driver firmware-misc-nonfree
 sudo dpkg --add-architecture i386 && sudo apt update
-sudo apt install nvidia-driver-libs:i386
-sudo apt install nvidia-cuda-dev nvidia-cuda-toolkit
+sudo apt install -y nvidia-driver-libs:i386
+sudo apt install -y nvidia-cuda-dev nvidia-cuda-toolkit
