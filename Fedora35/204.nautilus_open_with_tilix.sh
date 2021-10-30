e6c0fad12891408ba44c88181043a3b4d4d8245f
Tony Yousefnezhad <myousefnezhad@gmail.com>
2021-10-30 04:34:57 -0600

    some small changes for F35

--- Fedora35/204.nautilus_open_with_tilix.sh
+++ Fedora35/204.nautilus_open_with_tilix.sh
@@ -1,4 +1,4 @@
-sudo dnf install python3-nautilus tilix
+sudo dnf install python3-nautilus python3-pip tilix
 pip3 install --user nautilus-open-any-terminal
 glib-compile-schemas ~/.local/share/glib-2.0/schemas/
 gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal tilix
