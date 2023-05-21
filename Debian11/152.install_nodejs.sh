6f93d05ef2f078cd633251cbc575fbd73cb06bff
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-05-21 05:42:17 -0600

    correcting npm installation

--- Debian11/152.install_nodejs.sh
+++ Debian11/152.install_nodejs.sh
@@ -1,4 +1,9 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+export NVM_DIR="/home/tony/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install --lts
 sudo ln -sf /home/tony/.nvm /root/.nvm
-source ~/.zshrc
+npm install -g yarn
