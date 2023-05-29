b41ec32cb58808592c395a99ed9300047e1ea0ae
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-05-29 03:13:29 -0600

    neovim python3 issue

--- Debian11/156.neovim.sh
+++ Debian11/156.neovim.sh
@@ -6,9 +6,10 @@ sudo mkdir -p /var/lib/flatpak/exports/share/nvim
 sudo chmod -c 7777 -R /var/lib/flatpak/exports/share/nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim /var/lib/flatpak/exports/share/nvim/site/pack/packer/start/packer.nvim
 # Python Debuger
+sudo apt install python3 python3-pip python3-venv
 mkdir ~/.virtualenvs
 cd ~/.virtualenvs
-python -m venv debugpy
+python3 -m venv debugpy
 debugpy/bin/python -m pip install debugpy
 # Javascript Debugger
 git clone https://github.com/microsoft/vscode-js-debug ~/.vscode-js-debug
