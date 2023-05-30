a18cf3d1bf4eb6646cc5cae0554c0617e6db211e
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-05-30 04:06:53 -0600

    moving to AstroNvim

---
+++ Fedora38/171.install_powershell10k.sh
@@ -0,0 +1,1 @@
+sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/opt/oh-my-zsh/custom/themes/powerlevel10k
--- Fedora38/171.install_powershell9k.sh
+++
@@ -1,1 +0,0 @@
-sudo git clone https://github.com/bhilburn/powerlevel9k.git /home/common/oh-my-zsh/custom/themes/powerlevel9k
