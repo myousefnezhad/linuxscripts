9a4748145039b0cebffbbaa776fa29af9cce7442
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-05-30 04:47:32 -0600

    Fixing user dir in ANVIM

---
+++ nvim/lua/user/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+      "lua", "vimdoc", "javascript", "typescript", "c", "rust", "python", "go", "yaml", "regex", "r", "php", "matlab", "json", "html", "git_config", "git_rebase",  "gitcommit", "gitignore", "dockerfile", "cpp", "cuda", "bash", "css" 
+    })
+  end,
+}
---
+++ nvim/lua/user/plugins/user.lua
@@ -0,0 +1,15 @@
+return {
+  'mxsdev/nvim-dap-vscode-js',
+  'simrat39/rust-tools.nvim',
+  'airblade/vim-gitgutter',
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+}
