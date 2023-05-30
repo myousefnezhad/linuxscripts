a1d110559138514bc4c3848d808c39d668695082
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-05-30 16:02:54 -0600

    NVIM Dashbord

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -24,9 +24,9 @@ return {
       button("LDR n  ", get_icon("FileNew", 2, true) .. "New File  "),
       button("LDR f f", get_icon("Search", 2, true) .. "Find File  "),
       button("LDR f o", get_icon("DefaultFile", 2, true) .. "Recents  "),
-      button("LDR f w", get_icon("WordFile", 2, true) .. "Find Word  "),
-      button("LDR f '", get_icon("Bookmarks", 2, true) .. "Bookmarks  "),
-      button("LDR S l", get_icon("Refresh", 2, true) .. "Last Session  "),
+      --      button("LDR f w", get_icon("WordFile", 2, true) .. "Find Word  "),
+      --      button("LDR f '", get_icon("Bookmarks", 2, true) .. "Bookmarks  "),
+      --     button("LDR S l", get_icon("Refresh", 2, true) .. "Last Session  "),
     }
 
     dashboard.config.layout[1].val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) }
