3bc2fc476914f692743cca5d180443d90c6e6ffb
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-06-09 00:17:43 -0600

    F38

--- Fedora38/199.firewall.sh
+++ Fedora38/199.firewall.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #sudo firewall-cmd --remove-port=1025-65535/udp
 #sudo firewall-cmd --remove-port=1025-65535/tcp
-sudo firewall-cmd --permanent --add-service=samba
+#sudo firewall-cmd --permanent --add-service=samba
 sudo firewall-cmd --permanent --add-service=samba-client
-sudo firewall-cmd --permanent --add-service=http
+#sudo firewall-cmd --permanent --add-service=http
 #sudo firewall-cmd --permanent --add-service=mysql
-sudo firewall-cmd --permanent --add-port=1716/tcp
-sudo firewall-cmd --permanent --add-port=1716/udp
+# GSM
+#sudo firewall-cmd --permanent --add-port=1716/tcp
+#sudo firewall-cmd --permanent --add-port=1716/udp
+sudo firewall-cmd --permanent --add-port=3389/tcp
 sudo firewall-cmd --permanent --add-service=nfs
 sudo firewall-cmd --add-service={nfs3,mountd,rpc-bind} --permanent 
-sudo firewall-cmd --permanent --add-port=1365/tcp
+#sudo firewall-cmd --permanent --add-port=1365/tcp
 sudo firewall-cmd --reload
 sudo firewall-cmd --list-all
 
