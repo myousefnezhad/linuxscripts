4b380a98f06d1cfb91566747ac4a2f669e6ee99e
Tony M. Yousefnezhad <myousefnezhad@gmail.com>
2023-07-24 19:39:54 -0600

    Disable PostgresSQL backup to UOA

--- bin/backup_uoa.sh
+++ bin/backup_uoa.sh
@@ -3,5 +3,5 @@ LOG_PATH="/IRONMAN/logs/$LOG_FILE_FORMAT"
 echo $(date +%Y/%m/%d_%a_%H:%M:%S)  START >> "$LOG_PATH"
 rclone -v sync /IRONMAN/Documents/Topics uoa:/Topics
 rclone -v sync /IRONMAN/Datasets.bak     uoa:/Datasets.bak
-rclone -v sync /IRONMAN21/BackupPostgreSQL  uoa:/BackupPostgreSQL
+#rclone -v sync /IRONMAN21/BackupPostgreSQL  uoa:/BackupPostgreSQL
 echo $(date +%Y/%m/%d_%a_%H:%M:%S)  END >> "$LOG_PATH"
