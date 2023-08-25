4c55eb8aea9a15a4c9c44ac36132cd2926da3bb1
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-08-24 21:34:30 -0600

    NVIM SQL

--- nvim/lua/user/plugins/mason.lua
+++ nvim/lua/user/plugins/mason.lua
@@ -8,7 +8,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         "lua_ls", "tsserver", "html", "jsonls", "ltex", "remark_ls",
-        "yamlls", "rust_analyzer", "sqlls"
+        "yamlls", "rust_analyzer", "sqlls", "sqls"
 
       })
     end,
