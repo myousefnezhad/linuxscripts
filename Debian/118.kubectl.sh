aa059799dab485ce2527803c9e40b5cf6adb9e96
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-08-31 07:03:55 -0600

    kubectl auto completion

--- Debian/118.kubectl.sh
+++ Debian/118.kubectl.sh
@@ -4,3 +4,11 @@ chmod +x kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm ~/kubectl
 curl -sS https://webinstall.dev/k9s | bash
+
+
+sudo mkdir -p /home/common/oh-my-zsh/custom/plugins/kubectl-autocomplete/
+sudo kubectl completion zsh > /tmp/kubectl-autocomplete.plugin.zsh
+sudo cp /tmp/kubectl-autocomplete.plugin.zsh /home/common/oh-my-zsh/custom/plugins/kubectl-autocomplete/kubectl-autocomplete.plugin.zsh
+sudo mkdir -p /home/common/oh-my-zsh/completions
+sudo curl -L https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubectx.zsh -o /home/common/oh-my-zsh/completions/_kubectx.zsh
+sudo curl -L https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubens.zsh -o /home/common/oh-my-zsh/completions/_kubens.zsh
