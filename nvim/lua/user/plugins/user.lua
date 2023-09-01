3a0a35b91fe6659336fba40a47e239f66a9e1cdc
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-08-31 23:32:51 -0600

    setup cargo version for neovim

--- nvim/lua/user/plugins/user.lua
+++ nvim/lua/user/plugins/user.lua
@@ -4,6 +4,23 @@ return {
   'airblade/vim-gitgutter',
   'codota/tabnine-nvim',
   'tzachar/cmp-tabnine',
+  {
+    'saecki/crates.nvim',
+    ft = { 'rust', 'toml' },
+    config = function(_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   opts = function()
+  --     local M = require("plugins.cmd")
+  --     table.insert(M.source, { name = "crates" })
+  --     return M
+  --   end,
+  -- },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
