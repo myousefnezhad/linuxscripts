b11a4eaf6fb11015fdb3e515a77b483dc4d4f917
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-11-11 13:23:09 -0700

    add install helm

--- Arch/118.kubectl.sh
+++ Arch/118.kubectl.sh
@@ -4,3 +4,11 @@ chmod +x kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm ~/kubectl
 curl -sS https://webinstall.dev/k9s | bash
+
+
+cd ~
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+~/get_helm.sh
+rm ~/get_helm.sh
+
