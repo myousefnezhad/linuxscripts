288208233e427f9fb443eac56dec097d40c5cacf
Tony M. Yousefnezhad <myousefnezhad@gmail.com>
2024-02-26 04:30:27 -0700

    late changes

--- bin/backup_pgsql.sh
+++ bin/backup_pgsql.sh
@@ -1,6 +1,6 @@
 LOG_FILE_FORMAT=$(date +%Y.%m.pgsql.txt)
 LOG_PATH="/IRONMAN/logs/$LOG_FILE_FORMAT"
 echo $(date +%Y/%m/%d_%a_%H:%M:%S)  START >> "$LOG_PATH"
-pg_dump -U postgres -h 172.25.50.11 medai | gzip > /IRONMAN21/BackupPostgreSQL/medai_`date +%Y-%m-%d-%H-%M-%S`.sql.gz
-find /IRONMAN21/BackupPostgreSQL/. -mtime +7 | xargs rm -f
+pg_dump -U postgres -h db.cluster.learningbymachine.com -p 9999 uhrs | gzip > /IRONMAN1/Backups/BackupPostgreSQL/uhrs_`date +%Y-%m-%d-%H-%M-%S`.sql.gz
+find /IRONMAN1/Backups/BackupPostgreSQL/. -mtime +7 | xargs rm -f
 echo $(date +%Y/%m/%d_%a_%H:%M:%S)  END >> "$LOG_PATH"
