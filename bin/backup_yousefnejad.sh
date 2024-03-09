0cfcc47ee43f29bd417ef31804733c174435d39d
Tony M. Yousefnezhad <myousefnezhad@gmail.com>
2024-03-08 21:29:12 -0700

    Adding Topic Backup to Gmail

--- bin/backup_yousefnejad.sh
+++ bin/backup_yousefnejad.sh
@@ -2,6 +2,7 @@ LOG_FILE_FORMAT=$(date +%Y.%m.yousefnejad.txt)
 LOG_PATH="/IRONMAN/logs/$LOG_FILE_FORMAT"
 echo $(date +%Y/%m/%d_%a_%H:%M:%S)  START >> "$LOG_PATH"
 rclone -v sync /IRONMAN/Documents/myDocs gmail:/Datageddon/myDocs | logger
+rclone -v sync /IRONMAN/Documents/Topics gmail:/Datageddon/Topics | logger
 rclone -v sync /IRONMAN1/Backups         gmail:/Database.Backups
 echo $(date +%Y/%m/%d_%a_%H:%M:%S)  END >> "$LOG_PATH"
 
