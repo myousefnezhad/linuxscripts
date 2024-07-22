ae71876c7b8576dbd5625973eda3ece2853bb69e
Tony M. Yousefnezhad <myousefnezhad@gmail.com>
2024-07-22 00:35:30 -0600

    Moving backup from uofa to lbm

--- bin/backup_uoa.sh
+++ bin/backup_uoa.sh
@@ -1,7 +1,7 @@
 LOG_FILE_FORMAT=$(date +%Y.%m.uoa.txt)
 LOG_PATH="/IRONMAN/logs/$LOG_FILE_FORMAT"
-#echo $(date +%Y/%m/%d_%a_%H:%M:%S)  START >> "$LOG_PATH"
+echo $(date +%Y/%m/%d_%a_%H:%M:%S)  START >> "$LOG_PATH"
 #rclone -v sync /IRONMAN/Documents/Topics uoa:/Topics
-#rclone -v sync /IRONMAN/Datasets.bak     uoa:/Datasets.bak
+rclone -v sync /IRONMAN/Datasets.bak     lbm:/Datasets.bak
 #rclone -v sync /IRONMAN1/Backups         uoa:/Database.Backups
-#echo $(date +%Y/%m/%d_%a_%H:%M:%S)  END >> "$LOG_PATH"
+echo $(date +%Y/%m/%d_%a_%H:%M:%S)  END >> "$LOG_PATH"
