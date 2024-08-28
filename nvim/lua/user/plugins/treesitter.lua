2eb26578a49abe123e6ff7b7b5e4b466e6c57e96
Tony Yousefnezhad <myousefnezhad@gmail.com>
2024-08-28 05:30:35 -0600

    neovim sqls

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -4,7 +4,8 @@ return {
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       "lua", "vimdoc", "javascript", "typescript", "c", "rust", "python", "go", "yaml", "regex", "r", "php", "matlab",
-      "json", "html", "git_config", "git_rebase", "gitcommit", "gitignore", "dockerfile", "cpp", "cuda", "bash", "css"
+      "json", "html", "git_config", "git_rebase", "gitcommit", "gitignore", "dockerfile", "cpp", "cuda", "bash", "css",
+      "sql",
     })
   end,
   require 'nvim-treesitter.configs'.setup {
