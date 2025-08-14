31df3ef0fc78c1f56ea104536cde74fd1547f993
Tony Yousefnezhad <myousefnezhad@gmail.com>
2025-08-13 20:04:35 -0600

    F42

--- Fedora42/030.cuda_toolkit.sh
+++ Fedora42/030.cuda_toolkit.sh
@@ -1,5 +1,5 @@
-sudo dnf config-manager addrepo --from-repofile=https://developer.download.nvidia.com/compute/cuda/repos/fedora41/$(uname -m)/cuda-fedora41.repo
+sudo dnf config-manager addrepo --from-repofile=https://developer.download.nvidia.com/compute/cuda/repos/fedora42/$(uname -m)/cuda-fedora42.repo
 sudo dnf clean all
 sudo dnf module disable nvidia-driver
-sudo dnf config-manager setopt cuda-fedora41-$(uname -m).exclude=nvidia-driver,nvidia-modprobe,nvidia-persistenced,nvidia-settings,nvidia-libXNVCtrl,nvidia-xconfig
+sudo dnf config-manager setopt cuda-fedora42-$(uname -m).exclude=nvidia-driver,nvidia-modprobe,nvidia-persistenced,nvidia-settings,nvidia-libXNVCtrl,nvidia-xconfig
 sudo dnf -y install cuda-toolkit
